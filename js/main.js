1fb2174ec7690e05f4da0bd06774d2fe174156c2
Stanislav Maksarov <maksarov.s@gmail.com>
2017-05-01 01:22:33 -0700

    jQuery and CSS

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 $(function () {
 
-  console.log('This is Sparta!');
+  $('.sections').on('change', function(event) {
+    event.preventDefault();
+    
+    var section = this.value;
 
+    var urlNyt = "https://api.nytimes.com/svc/topstories/v2/" + section + ".json?api-key=7f1dbfd97259499a96059cbfe5221ff3";
+    var newsDisplayed = "";
+    var selection = $('.section').val();
+    $('.articles').empty();
+    $('.loader').show();
+
+    $.ajax({
+      method: 'GET',
+      url: urlNyt,
+    })
+    
+    .done(function(data) {
+      var imagesTrue = data.results.filter(function(imagesFilter){
+        return imagesFilter.multimedia.length > 0;
+      }).slice(0,12)
+      $.each(imagesTrue, function(index, value) {
+        newsDisplayed += '<li class="news-list">',
+        newsDisplayed += '<img src="' + value.multimedia[4].url + '">',
+        newsDisplayed += '<p>' + value.abstract + '</p>',
+        newsDisplayed += '</li>';
+        // console.log(newsDisplayed);
+      });
+        $('.articles').append(newsDisplayed);
+
+    }).always(function() {
+      $('.loader').hide();
+    });
+    
+  });
+ 
 });
